a89750466848730ec6256340d4655c860c415c77
Ivan Ustyantsev <vangold915@gmail.com>
2021-11-08 00:16:48 +0700

    js: added click handler

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var btn = document.getElementById('btn');
+var btnButton = document.getElementById('btn-button');
+var counterVal = 0;
+
+
+function myReadData() {
+    var name = document.getElementById('form').elements[0].value;
+    var email = document.getElementById('form').elements[1].value;
+    var phone = document.getElementById('form').elements[2].value;
+    var answer = document.getElementById('form').elements[3].value;
+
+    if (name == "" || email == "" || phone == "" || answer == "") {
+        alert ("Вы не до конца ввели данные в форму. Пожалуйста, заполните все поля!")
+    } else {
+        alert(`Ваш E-mail: ${email}, телефон: ${phone}. Спасибо ${name}! Смело двигайтесь к вашей мечте! Приятного полета в ${answer}!`);
+    }
+}
+
+btnButton.onclick = function () {
+
+    myReadData();
+}
+
+btn.onclick = function () {
+    ++counterVal;
+    if (counterVal == 1) {
+        document.querySelector(".intro").innerHTML = "Вы можете создавать такие же лэндинги, пройдя курс от команды WayUp";
+        var button = document.querySelector('.btn')
+        button.classList.add('green');
+        document.getElementById('btn').innerHTML = "На сайт WAYUP";
+    }
+    if (counterVal == 2) {
+        document.getElementById('btn').setAttribute("href","https://wayup.in/")
+    }
+}
